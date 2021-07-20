af468457fc4d40dfd1b4c5e079093e8ed609319f
jjmorales <jjmorales879@gmail.com>
2021-07-20 17:15:27 -0400

    added changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+export const App = () => {
+  const [task, setTask] = useState("");
+  const [list, setList] = useState([{ label: "", done: false }]);
+
+  const handleChange = (e) => {
+    setTask(e.target.value);
+  };
+
+  const handleAdd = (event) => {
+    if (task.length > 2) {
+      setList(list.concat({ label: task, done: false }));
+    } else {
+      alert("task must contain more than two letters");
+    }
+    setTask("");
+    event.preventDefault();
+  };
+
+  const handleDelete = (id) => {
+    const newList = list.filter((task, index) => index !== id);
+    setList(newList);
+  };
+
+  const deleteList = () => {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/juanm", {
+      method: "DELETE",
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const updateList = () => {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/juanm", {
+      method: "PUT",
+      body: JSON.stringify(list),
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        return response.json;
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const getList = () => {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/juanm")
+      .then((res) => res.json())
+      .then((data) => setList(data));
+  };
+
+  const createList = () => {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/juanm", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify([{ label: "sample task", done: false }]),
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    updateList();
+  }, [list]);
+
+  useEffect(() => {
+    getList();
+  }, []);
+
+  //createList()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form onSubmit={handleAdd}>
+        <input
+          value={task}
+          type="text"
+          placeholder="Add to-do"
+          onChange={handleChange}
+        />
+      </form>
+      {list.length > 0 ? (
+        <ul>
+          {list.map((task, index) => (
+            <li key={index}>
+              {task.label}
+              <button onClick={() => handleDelete(index)}>X</button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No new taks</p>
+      )}
+      <p> {list.length} to be completed</p>
+      <button type="submit" onClick={deleteList}>
+        Delete All
+      </button>
+      <button type="submit" onClick={createList}>
+        Create list
+      </button>
     </div>
   );
-}
-
+};
 export default App;
